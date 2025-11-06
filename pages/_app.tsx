9177a01e1b2e642198808bbf0107d1bd0835bf51
abdul-28930 <oisci0793@gmail.com>
2025-11-06 13:18:34 +0530

    Add Facebook Pixel tracking to MyApp component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,47 @@
 import '../global.css';
 import type { AppProps } from 'next/app';
+import Script from 'next/script';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        {/* Meta Pixel Code - Noscript */}
+        <noscript>
+          <img 
+            height="1" 
+            width="1" 
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=1539209977275943&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+      </Head>
+
+      {/* Meta Pixel Code - Script */}
+      <Script
+        id="facebook-pixel"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1539209977275943');
+            fbq('track', 'PageView');
+          `,
+        }}
+      />
+      
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
